d0b897853ea6468248952884a15bdbf680e8e60b
leonp5 <leonpe@web.de>
2019-12-10 21:18:31 +0100

    Add styling and create basic add ToDo function

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Global, css } from "@emotion/core";
+import { useTheme } from "emotion-theming";
 
 export default function GlobalStyles() {
+  const theme = useTheme();
   return (
     <Global
       styles={css`
@@ -9,6 +11,14 @@ export default function GlobalStyles() {
         *::after,
         *::before {
           box-sizing: border-box;
+        }
+        body {
+          height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin: 0;
+          background: ${theme.background};
         }
       `}
     />
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,7 +1,14 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const Item = styled.div``;
+const Item = styled.div`
+  width: 90%;
+  padding: 10px;
+  margin: 4px;
+  border-radius: 5px;
+  color: ${prop => prop.theme.secondary};
+  background: ${prop => prop.theme.tertiary};
+`;
 
 function ToDo({ todo, index }) {
   return <Item>{todo.text}</Item>;
